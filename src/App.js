ba43664b0a120e056c3109d01bbdf48f7bca9202
Pietro Ghezzi <pietro.ghezzi@eoc.ch>
2018-10-29 16:46:14 +0100

    working version

--- src/App.js
+++ src/App.js
@@ -1,64 +1,70 @@
-import React, { Component } from 'react';
-import { Container, Col, Row, Jumbotron} from 'reactstrap'
-import * as tf from '@tensorflow/tfjs';
+import React, { Component } from "react";
+import { Container, Col, Row, Jumbotron } from "reactstrap";
+import { train, predict } from "./trainModel";
 
-import * as d3 from "d3"
-
-const lineFunction = d3.line()
-                         .x(function(d) { return d.x; })
-                         .y(function(d) { return d.y; })
-
-const normalize = (width, height) => (x, y) => ({x: x/width, y: (height-y)/height})
-const denormalize = (width, height) => (x, y) => ({x: x*width, y: -1*(height*y-height)})
+const normalize = (width, height) => (x, y) => ({
+  x: x / width,
+  y: (height - y) / height
+});
+const denormalize = (width, height) => (x, y) => ({
+  x: x * width,
+  y: -1 * (height * y - height)
+});
 
 class App extends Component {
-
   state = {
     points: []
-  }
+  };
 
-  learningRate = 0.2
-  optimizer = tf.train.sgd(this.learningRate)
-  m = tf.variable(tf.scalar(Math.random()))
-  b = tf.variable(tf.scalar(Math.random()))
+  addCircle = (coordX, coordY) => {
+    const { x, y, width, height } = this.svgContainer.getBoundingClientRect();
+    const { points } = this.state;
+    const normalized = normalize(width, height)(coordX - x, coordY - y);
+    const newPoint = { x: normalized.x, y: normalized.y };
+    this.setState({ points: [...points, newPoint] });
+  };
 
-  loss(pred, label) {
-    return pred.sub(label).square().mean()
-  }
-
-  train() {
-    const { points } = this.state
-    const ys = tf.tensor1d(points.map(point => point.y))
-    this.optimizer.minimize(() => this.loss(this.predict(points.map(point => point.x)), ys))
-  }
-
-  predict(x) {
-    const xs = tf.tensor1d(x);
-    // linear function: y = m*x + b
-    const ys = xs.mul(this.m).add(this.b)    
-    return ys
-  }
-
-  addCircle(coordX, coordY) {
-    const {x, y, width, height} = this.svgContainer.getBoundingClientRect()
-    const { points } = this.state
-    const normalized = normalize(width, height)(coordX - x, coordY - y)
-    const newPoint = {x: normalized.x, y: normalized.y}
-    this.setState({ points: [...points, newPoint] })    
-  }
-
-  draw() {
-    if(this.svgContainer) {
-      const { points } = this.state    
+  draw = () => {
+    if (this.svgContainer) {
+      const { points } = this.state;
       const svgContainer = this.svgContainer.getBoundingClientRect();
       return points.map(point => {
-        const {x, y} = denormalize(svgContainer.width, svgContainer.height)(point.x, point.y)        
-        return <Circle key={`point-${x}-${y}`} x={x} y={y} />
-      })
-    }    
-  }
+        const { x, y } = denormalize(svgContainer.width, svgContainer.height)(
+          point.x,
+          point.y
+        );
+        return <Circle key={`point-${x}-${y}`} x={x} y={y} />;
+      });
+    }
+  };
+
+  drawLine = () => {
+    const svgContainer = this.svgContainer.getBoundingClientRect();
+    const xs = [0, 1];
+    const ys = predict(xs).dataSync();
+
+    const { x: x1, y: y1 } = denormalize(
+      svgContainer.width,
+      svgContainer.height
+    )(xs[0], ys[0]);
+
+    const { x: x2, y: y2 } = denormalize(
+      svgContainer.width,
+      svgContainer.height
+    )(xs[1], ys[1]);
+    return (
+      <line x1={x1} y1={y1} x2={x2} y2={y2} stroke="orange" strokeWidth="2" />
+    );
+  };
 
   render() {
+    const { points } = this.state;
+    if (points.length > 1) {
+      for (let i = 0; i < 10; i++) {
+        train(points);
+      }
+    }
+
     return (
       <Container>
         <Jumbotron>
@@ -66,13 +72,16 @@ class App extends Component {
         </Jumbotron>
         <Row>
           <Col xs={12}>
-          <svg ref={(e) => this.svgContainer = e } 
-            width="100%" height={500} 
-            style={{background:'red'}} 
-            onMouseDown={(e) => this.addCircle(e.clientX, e.clientY)}>
-              { this.draw() }
-              {/* <path d={lineFunction(points)}/> */}
-          </svg>          
+            <svg
+              ref={e => (this.svgContainer = e)}
+              width="100%"
+              height={500}
+              style={{ background: "grey" }}
+              onMouseDown={e => this.addCircle(e.clientX, e.clientY)}
+            >
+              {this.draw()}
+              {points.length > 1 ? this.drawLine() : null}
+            </svg>
           </Col>
         </Row>
       </Container>
@@ -82,5 +91,6 @@ class App extends Component {
 
 export default App;
 
-const Circle = ({x, y}) => <circle cx={x} cy={y} r="4" stroke="black" strokeWidth="1" fill="yellow"/>
-
+const Circle = ({ x, y }) => (
+  <circle cx={x} cy={y} r="4" stroke="black" strokeWidth="1" fill="yellow" />
+);
---
+++ src/trainModel.js
@@ -0,0 +1,26 @@
+import * as tf from "@tensorflow/tfjs";
+
+const learningRate = 0.2;
+const optimizer = tf.train.sgd(learningRate);
+var m = tf.variable(tf.scalar(Math.random()));
+var b = tf.variable(tf.scalar(Math.random()));
+
+const loss = (pred, label) =>
+  pred
+    .sub(label)
+    .square()
+    .mean();
+
+const train = points => {
+  const ys = tf.tensor1d(points.map(point => point.y));
+  optimizer.minimize(() => loss(predict(points.map(point => point.x)), ys));
+};
+
+const predict = x => {
+  const xs = tf.tensor1d(x);
+  // linear function: y = m*x + b
+  const ys = xs.mul(m).add(b);
+  return ys;
+};
+
+export { train, predict };
